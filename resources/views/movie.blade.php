90a204a2e5c74ee3af99b71aa9c54ea0acbeffea
Bui Thanh Cong <101445814+cong2510@users.noreply.github.com>
2023-03-28 23:23:10 +0700

    trailer

--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -65,9 +65,10 @@
                         <span class="col-left">Thể loại: </span>
                         @foreach ($theloais as $theloai)
                             <span class="col-right">
-                            @if ($theloai['IDTheLoai'] == $movie['IDTheLoai'])
-                                {{ $theloai['TenTheLoai'] }}
-                            @endif</span>
+                                @if ($theloai['IDTheLoai'] == $movie['IDTheLoai'])
+                                    {{ $theloai['TenTheLoai'] }}
+                                @endif
+                            </span>
                         @endforeach
                     </li>
                     <li>
@@ -80,7 +81,7 @@
                     </li>
                     <li>
                         <span class="col-left">
-                            <button type="button" class="btn btn-muave" style="background: #66CC00;margin-right: 10px">
+                            <button type="button" class="btn btn-muave" style="background: #66CC00;margin-right: 10px" data-bs-toggle="collapse" data-bs-target="#trailer">
                                 <a href="#"
                                     style="font-weight: bold;color: white;text-decoration: none;font-size: 14px;padding: 8px;">XEM
                                     TRAILER</a>
@@ -98,8 +99,14 @@
             </div>
         </div>
     </div>
-    <div class="container">
-
+    <div class="container collapse" style="width: 90%" id="trailer">
+        <div style="text-align: center">
+            <h1>{{ $movie['TenPhim'] }}</h1>
+        </div>
+        <div class="ratio ratio-16x9">
+            <iframe class="embed-responsive-item" src="{{ asset('trailer/' .$movie['Trailer']) }}" id="video"
+                allowscriptaccess="always" sandbox></iframe>
+        </div>
     </div>
     <x-event />
     <x-footer />
