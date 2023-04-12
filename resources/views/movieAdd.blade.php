9094b360af5d0b37c3ddffe15fc363515a1ed88a
phanngocson0407 <sonfpt149@gmail.com>
2023-04-12 20:25:38 +0700

    t vừa gộp

--- resources/views/movieAdd.blade.php
+++ resources/views/movieAdd.blade.php
@@ -1,56 +1,6 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <title>Thêm/Sửa Sản Phẩm</title>
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
-
-    <!-- jQuery library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-
-    <!-- Popper JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-
-    <!-- Latest compiled JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-
-    <!-- summernote -->
-    <!-- include summernote css/js -->
-    <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
-</head>
-
-<body>
-    <div>
-        <ul class="nav nav-tabs row">
-            <li class="nav-item col-md-5">
-                <a class="nav-link  " href="{{ route('movieAdmin') }}">xin chào</a>
-            </li>
-
-            <li class="nav-item  col-md-6 d-flex" style="">
-
-                <a class="nav-link" href="">Đăng Xuất</a>
-            </li>
-        </ul>
-        <ul class="nav nav-tabs row">
-            <li class="nav-item col-md-2">
-                <a class="nav-link  " href="{{ route('movieCategory') }}">Quản Lý Loại Phim</a>
-            </li>
-            <li class="nav-item col-md-2">
-                <a class="nav-link " href="">Quản Lý Phim</a>
-            </li>
-            <li class="nav-item col-md-2">
-                <a class="nav-link " href=""> Quản Lí Người Dùng</a>
-            </li>
-            <li class="nav-item col-md-2">
-                <a class="nav-link " href="{{ route('movieTicket') }}"> Quản Lí Vé</a>
-            </li>
-
-        </ul>
-    </div>
-    <div class="container">
+@extends('dashboard')
+@section('admin')
+<div class="container">
         <div class="panel panel-primary">
             <div class="panel-heading">
                 <h2 class="text-center">Thêm/Sửa Sản Phẩm</h2>
@@ -99,7 +49,4 @@
             </div>
         </div>
     </div>
-
-</body>
-
-</html>
+    @endsection
--- resources/views/movieTicket.blade.php
+++ resources/views/movieTicket.blade.php
@@ -1,52 +1,6 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <title> a</title>
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
-
-    <!-- jQuery library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-
-    <!-- Popper JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-
-    <!-- Latest compiled JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-</head>
-
-<body>
-
-    <div>
-        <ul class="nav nav-tabs row">
-            <li class="nav-item col-md-5">
-                <a class="nav-link  " href="{{ route('movieAdmin') }}">xin chào</a>
-            </li>
-
-            <li class="nav-item  col-md-6 d-flex" style="">
-
-                <a class="nav-link" href="">Đăng Xuất</a>
-            </li>
-        </ul>
-        <ul class="nav nav-tabs row">
-            <li class="nav-item col-md-2">
-                <a class="nav-link  " href="{{ route('movieCategory') }}">Quản Lý Loại Phim</a>
-            </li>
-            <li class="nav-item col-md-2">
-                <a class="nav-link " href="">Quản Lý Phim</a>
-            </li>
-            <li class="nav-item col-md-2">
-                <a class="nav-link " href="{{ route('movieUser') }}"> Quản Lí Người Dùng</a>
-            </li>
-            <li class="nav-item col-md-2">
-                <a class="nav-link " href="{{ route('movieTicket') }}"> Quản Lí Vé</a>
-            </li>
-
-        </ul>
-    </div>
-    <div class="container">
+@extends('dashboard')
+@section('admin')
+<div class="container">
         <div class="panel panel-primary">
             <div class="panel-heading">
                 <h2 class="text-center">Quản Lý Vé</h2>
@@ -94,6 +48,4 @@
             </div>
         </div>
     </div>
-</body>
-
-</html>
+@endsection
